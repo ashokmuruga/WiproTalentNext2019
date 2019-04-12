f539110758a992db579738e1e05c0e6e552a41b8
Ashok M <hai2ashok@yahoo.co.in>
2019-04-12 11:51:15 +0530

    Create FlowControl12.java

---
+++ FlowControl12.java
@@ -0,0 +1,25 @@
+public class MyClass {
+    public static void main(String args[]) {
+   
+    int num=Integer.parseInt(args[0]);
+    
+     boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            // condition for nonprime number
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
+    
+    
+   
+}
+}
