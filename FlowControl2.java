f857cf0999a98bf0ee4c6fedbd10e922c843a778
Ashok M <hai2ashok@yahoo.co.in>
2019-04-12 10:19:07 +0530

    Create FlowControl2.java

---
+++ FlowControl2.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String args[]) {
+    
+    int num1=Integer.parseInt(args[0]);
+   
+    if(num1%2==0)
+      System.out.print(num1+"is even");
+    else
+      System.out.print(num1+"is odd");
+    
+          }
+}
