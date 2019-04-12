f30f0a5cb9b78041c1773d190593849eee88f14d
Ashok M <hai2ashok@yahoo.co.in>
2019-04-12 10:38:29 +0530

    Create FlowControl3.java

---
+++ FlowControl3.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String args[]) {
+    
+    if(args.length<=0)
+       System.out.print("No Values");
+    else
+    {
+     for(int i=0;i<args.length;i++)  
+        System.out.print(args[i]+",");  
+     }
+}
+}
