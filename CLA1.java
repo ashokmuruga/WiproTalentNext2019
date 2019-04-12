7b8cfebd323fc824ef18b5849702bfba85d4225d
Ashok M <hai2ashok@yahoo.co.in>
2019-04-12 10:05:05 +0530

    Create CLA1.java

---
+++ CLA1.java
@@ -0,0 +1,9 @@
+//CLA
+public class MyClass {
+    public static void main(String args[]) {
+      String str1=args[0];
+      String str2=args[1];
+      
+      System.out.print(str1+"Technologies"+str2);
+          }
+}
