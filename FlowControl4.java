04bac88f37b61465ea4a8f7ff28a6ac23f474c6c
Ashok M <hai2ashok@yahoo.co.in>
2019-04-12 10:55:43 +0530

    Create FlowControl4.java

---
+++ FlowControl4.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String args[]) {
+    
+   char a='z';
+   char b='s';
+   
+   if((int)a<(int)b)
+      System.out.print(a+","+b);
+   else
+      System.out.print(b+","+a);
+}
+}
