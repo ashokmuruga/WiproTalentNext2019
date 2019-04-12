f17a9400c8a8851e9353b5e42a252f595ce7aaac
Ashok M <hai2ashok@yahoo.co.in>
2019-04-12 10:11:36 +0530

    Create CLA3.java

---
+++ CLA3.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String args[]) {
+    
+    int num1=Integer.parseInt(args[0]);
+    int num2=Integer.parseInt(args[1]);  
+      System.out.print("The sum of"+num1+"and"+num2+"is "+(num1+num2));
+          }
+}
