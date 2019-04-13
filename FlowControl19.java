f58c7bfcf8a977f87f944b5728fb877023b5f75f
Ashok M <hai2ashok@yahoo.co.in>
2019-04-13 12:07:24 +0530

    Create FlowControl19.java

---
+++ FlowControl19.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String args[]) {
+        for (int num = 0; num < 150; num++) 
+        {      
+            
+            if (num % 2 == 0&&num % 3 == 0 && num % 5 == 0) 
+                System.out.print(num + " "); 
+        } 
+    }
+}
