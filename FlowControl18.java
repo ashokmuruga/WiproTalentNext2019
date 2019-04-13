c376190ac24b1ccf67fcac828044c5dc0a376932
Ashok M <hai2ashok@yahoo.co.in>
2019-04-13 12:02:41 +0530

    Create FlowControl18.java

---
+++ FlowControl18.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        int n, m, a = 0,x;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        m = n;
+        while(n > 0)
+        {
+            x = n % 10;
+            a = a * 10 + x;
+            n = n / 10;
+        }
+       if(a == m)
+        {
+            System.out.println(m+" is Palindrome");
+        }
+        else
+        {
+            System.out.println(m+" is Not Palindrome");
+        }
+    }
+}
