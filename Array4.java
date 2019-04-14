a380e135ccf605a8236296c32929407986049e0c
Ashok M <hai2ashok@yahoo.co.in>
2019-04-14 11:50:56 +0530

    Create Array4.java

---
+++ Array4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class MyClass {
+    public static void main(String args[]) {
+        int a[]={67,101,72,113,74};
+        for(int i=0;i<5;i++)
+          System.out.print((char)a[i]+"\t");
+    }
+}
