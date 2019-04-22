17cdaae24f72682648c6b2aed1dba6746f9adc94
Ashok M <hai2ashok@yahoo.co.in>
2019-04-22 09:55:21 +0530

    Create Collections1.java

---
+++ Collections1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) 
+    {
+        ArrayList<String> arrlist1 = new ArrayList<String>(12);
+        arrlist1.add("January");
+        arrlist1.add("February");
+        arrlist1.add("March");
+        arrlist1.add("April");
+        arrlist1.add("May");
+        arrlist1.add("June");
+        arrlist1.add("July");
+        arrlist1.add("August");
+        arrlist1.add("September");
+        arrlist1.add("October");
+        arrlist1.add("November");
+        arrlist1.add("December");
+        
+       
+       System.out.println(arrlist1);
+    }
+}
