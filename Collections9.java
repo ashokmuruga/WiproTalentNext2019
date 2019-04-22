f262599ebc2fcb3407161b7cdf2b465047c0ace1
Ashok M <hai2ashok@yahoo.co.in>
2019-04-22 15:24:31 +0530

    Create Collections9.java

---
+++ Collections9.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) 
+    {
+        HashSet<String> arrlist1 = new HashSet<String>(12);
+        arrlist1.add("January");
+        arrlist1.add("February");
+        arrlist1.add("March");
+        arrlist1.add("April");
+        arrlist1.add("May");
+        arrlist1.add("June");
+        arrlist1.add("July");
+        arrlist1.add("August");
+        arrlist1.add("September");
+        arrlist1.add("October");
+        arrlist1.add("November");
+        arrlist1.add("December");
+        
+        
+        TreeSet<String> obj=new TreeSet<String>(arrlist1); 
+        
+        Iterator itr=obj.iterator();
+        while(itr.hasNext())
+        {
+         System.out.println(itr.next());
+        } 
+    }
+}
