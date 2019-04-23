6e5652624cf09460250f2f38f70f60694f0760d5
Ashok M <hai2ashok@yahoo.co.in>
2019-04-23 11:34:00 +0530

    Create MT1.java

---
+++ MT1.java
@@ -0,0 +1,22 @@
+public class TestMultiNaming1 extends Thread
+{  
+  public void run()
+  {  
+   System.out.println("running...");  
+  }  
+ public static void main(String args[])
+ {  
+  TestMultiNaming1 t1=new TestMultiNaming1();  
+  TestMultiNaming1 t2=new TestMultiNaming1();  
+  System.out.println("Name of t1:"+t1.getName());  
+  System.out.println("Name of t2:"+t2.getName());  
+   
+  t1.start();  
+  t2.start();  
+  
+  t1.setName("Scooby");
+    t2.setName("Shaggy");
+  System.out.println("After changing name of t1:"+t1.getName());  
+  System.out.println("After changing name of t2:"+t2.getName()); 
+ }  
+}  
